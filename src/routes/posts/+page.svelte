decb5969ada600bd9f1e280f764aa464c60d6540
Christian Munch <chri779r@stud.kea.dk>
2023-01-18 19:57:08 +0100

    paths

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,20 +1,6 @@
 <script context="module">
-	import { client } from '$lib/graphql-client';
-	import { postsQuery } from '$lib/graphql-queries';
 	import { marked } from 'marked';
 
-	export const load = async () => {
-		const { posts } = await client.request(postsQuery);
-
-		return {
-			props: {
-				posts
-			}
-		};
-	};
-</script>
-
-<script>
 	export let posts;
 </script>
 
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,20 +1,6 @@
 <script context="module">
 	import ProjectCard from '$lib/components/projectCard/projectCard.svelte';
-	import { client } from '$lib/graphql-client';
-	import { projectsQuery } from '$lib/graphql-queries';
 
-	export const load = async () => {
-		const { projects } = await client.request(projectsQuery);
-
-		return {
-			props: {
-				projects
-			}
-		};
-	};
-</script>
-
-<script>
 	export let projects;
 </script>
 
